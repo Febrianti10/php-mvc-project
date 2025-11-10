e12cdbe08ace2b7f235dcafb7ca3e4331c64bdbc
Febrianti10 <fbrntkhmrh@gmail.com>
2025-11-10 08:20:06 +0700

    up

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'host' => getenv('DB_HOST') ?: 'localhost',
-    'port' => getenv('DB_PORT') ?: '3306',
+    'port' => getenv('DB_PORT') ?: '8080',
     'dbname' => getenv('DB_NAME') ?: 'mvc_db',
     'username' => getenv('DB_USER') ?: 'root',
     'password' => getenv('DB_PASSWORD') ?: '',
